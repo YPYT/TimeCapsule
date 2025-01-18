889d7e86d74860ee66c676e58efdad81ce2d0aaf
yuka <yka723158@gmail.com>
2025-01-18 23:50:25 +1100

    basic function in the create capsule page

--- lib/create_capsule.dart
+++ lib/create_capsule.dart
@@ -1,12 +1,68 @@
 import 'package:flutter/material.dart';
 
-class CreateScreen extends StatelessWidget {
+class CreateScreen extends StatefulWidget {
   const CreateScreen({super.key});
+
+  @override
+  State<CreateScreen> createState() => _CreateScreenState();
+}
+
+class _CreateScreenState extends State<CreateScreen> {
+  DateTime? _selectedDate = null;
+  TextEditingController _dateController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Create Screen'),
+      child: Column(
+        children: [
+          TextField(
+            controller: _dateController,
+            decoration: InputDecoration(
+              labelText: 'DATE',
+              filled: true,
+              prefixIcon: Icon(Icons.calendar_today),
+            ),
+            readOnly: true,
+            onTap: (){
+              _selectDate(context);
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Message',
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Address',
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+            ),
+            onPressed: () {},
+            child: Text("Bury!"),
+          ),
+        ],
+      )
     );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2200)
+    );
+
+    if (picked != null) {
+      _selectedDate = picked;
+      setState(() {
+        _dateController.text = picked.toString().split(" ")[0];
+      });
+    }
   }
 }
