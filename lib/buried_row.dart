9f3d9aaeaf8eda20f8966b40c28202402726ba9e
yuka <yka723158@gmail.com>
2025-01-23 00:32:50 +1100

    Layout for buried row

--- lib/buried_row.dart
+++ lib/buried_row.dart
@@ -14,6 +14,9 @@ class BuriedRow extends StatefulWidget {
 class _BuriedRowState extends State<BuriedRow> {
   @override
   Widget build(BuildContext context) {
+    DateTime buriedDate = DateTime.parse(widget.capsule["buried_date"]);
+    DateTime unlockDate = DateTime.parse(widget.capsule["date"]);
+
     return GestureDetector(
       onTap: () {
         Navigator.push(context, MaterialPageRoute(builder: (context) => UnlockedCapsuleScreen(capsule: widget.capsule)));
@@ -28,10 +31,25 @@ class _BuriedRowState extends State<BuriedRow> {
           border: Border.all(color: Colors.black, width: 1.5),
           borderRadius: BorderRadius.circular(10),
         ),
-        child: Column(
-            children: [
-              Text("Title: ${widget.capsule["title"]}, Message: ${widget.capsule["message"]}, Date: ${widget.capsule["date"]}, Address: ${widget.capsule["address"]}"),
-            ]
+        child: Row(
+          spacing: 50,
+          children: [
+            Column(
+              children: [
+                Image(
+                  image: AssetImage("assets/rabbit.png"),
+                ),
+                Text("Myself"),
+              ],
+            ),
+            Column(
+                children: [
+                  Text("Title: ${widget.capsule["title"]}"),
+                  Text("Unlock: ${unlockDate.day}/${unlockDate.month}/${unlockDate.year}"),
+                  Text("Buried: ${buriedDate.day}/${buriedDate.month}/${buriedDate.year}"),
+                ]
+            ),
+          ],
         ),
       ),
     );
