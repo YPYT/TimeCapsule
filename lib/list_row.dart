3b3a585b449f5c7af6605bf10a385c16ff9ea0ef
yuka <yka723158@gmail.com>
2025-01-25 04:03:08 +1100

    View list row layout

--- lib/list_row.dart
+++ lib/list_row.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:time_machine/time_machine.dart';
+import 'package:intl/intl.dart';
+import 'names.dart';
 
 class ListRow extends StatefulWidget {
   final dynamic capsule;
@@ -16,15 +18,16 @@ class _ListRowState extends State<ListRow> {
   Widget build(BuildContext context) {
     LocalDate unlockDate = LocalDate.dateTime(DateTime.parse(widget.capsule["date"]));
     Period dateDiff = unlockDate.periodSince(LocalDate.today());
-    String dateDiffString = "years: ${dateDiff.years}; months: ${dateDiff.months}; days: ${dateDiff.days}";
     DateTime buriedDate = DateTime.parse(widget.capsule["buried_date"]);
+
+    NumberFormat formatter = NumberFormat("00");
 
     return Container(
       height: 130,
       width: double.infinity,
       alignment: Alignment.center,
       margin: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 0),
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.only(top: 20, left: 20),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black, width: 1.5),
         borderRadius: BorderRadius.circular(10),
@@ -32,27 +35,57 @@ class _ListRowState extends State<ListRow> {
       child: Column(
           children: [
             Row(
+              spacing: 30,
               children: [
                 Image(
                   image: AssetImage("assets/${widget.capsule["capsule_image"]}"),
                 ),
-                Text(dateDiffString),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 30,
+                  children: [
+                    Column(
+                      children: [
+                        Text(formatter.format(dateDiff.years), style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
+                        )),
+                        Text("Years")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(formatter.format(dateDiff.months), style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
+                        )),
+                        Text("Months")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(formatter.format(dateDiff.days), style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold
+                        )),
+                        Text("Days")
+                      ],
+                    )
+                  ],
+                ),
               ],
             ),
             Row(
-              spacing: 20,
+              spacing: 50,
               children: [
-                Text("Myself"),
-                Text("Buried: ${buriedDate.day}/${buriedDate.month}/${buriedDate.year}"),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: CircleBorder(),
-                    backgroundColor: Colors.yellow,
-                    foregroundColor: Colors.red,
-                  ),
-                  child: Icon(Icons.place, color: Colors.black),
+                Padding(
+                  padding: const EdgeInsets.only(left: 3),
+                  child: Text(getName(widget.capsule["sender"]), style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  )),
                 ),
+                Text("Buried: ${buriedDate.day}/${buriedDate.month}/${buriedDate.year}"),
               ],
             ),
           ]
